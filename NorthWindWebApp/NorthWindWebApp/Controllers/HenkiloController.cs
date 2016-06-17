2bf0e8fc84bd6aebeadc8ab89d110db427137238
Juha Sonck <juha.sonck@pointcollege.fi>
2016-06-17 12:46:31 +0300

    Paljon kehitystä

---
+++ NorthWindWebApp/NorthWindWebApp/Controllers/HenkiloController.cs
@@ -0,0 +1,54 @@
+﻿using NorthWindWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthWindWebApp.Controllers
+{
+    public class HenkiloController : Controller
+    {
+        // GET: Henkilo
+        public ActionResult Index()
+        {
+            
+
+            ProjektitEntities entities = new ProjektitEntities();
+            List<Henkilot> model = entities.Henkilot.ToList();
+            entities.Dispose();
+
+            return View(model);
+        }
+        public ActionResult HaeTunnit(string id)
+        {
+            int iid;
+            iid = int.Parse(id);
+            DateTime vertailuPvm = DateTime.Today.AddDays(-150);
+            //vertailuPvm = Convert.ToDateTime("01/05/2016");
+
+            ProjektitEntities entities = new ProjektitEntities();
+            List<Tunnit> tunnit = (from tt in entities.Tunnit
+                                   where tt.Henkilo_id == iid
+                                   && tt.Pvm > vertailuPvm
+                                   select tt).ToList();
+            entities.Dispose();
+
+            List<SimpleTuntiData> result = new List<SimpleTuntiData>();
+            foreach (Tunnit tunti in tunnit)
+            {
+                SimpleTuntiData data = new SimpleTuntiData();
+                data.tunnit = tunti.Tunnit1.ToString();
+                data.paivamaara = tunti.Pvm.ToString();
+                data.henkilonumero = tunti.Henkilo_id.ToString();
+                data.projektinumero = tunti.Projekti_id.ToString();
+
+                result.Add(data);
+
+            }
+
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ NorthWindWebApp/NorthWindWebApp/Models/SimpleTuntiData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NorthWindWebApp.Models
+{
+    public class SimpleTuntiData
+    {
+        public string projektinumero { get; set; }
+        public string projektinimi { get; set; }
+        public string henkilonumero { get; set; }
+        public string henkilonimi { get; set; }
+        public string paivamaara { get; set; }
+        public string klo { get; set; }
+        public string tunnit { get; set; }
+
+    }
+}
